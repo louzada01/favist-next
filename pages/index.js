bb5922e35dc9aa678fca9d8b6d8d82cffbcec7c5
louzada01 <silvalouzada@gmail.com>
2020-10-01 00:55:17 -0300

    :tada: Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Favist</h1>
+
+        <p className={styles.description}>
+          Sua lista para os piores momentos
+        </p>
+
+        <a>Add Moment</a>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Feito com 💛 by Louzada
+        </a>
+      </footer>
+    </div>
+  );
+}
